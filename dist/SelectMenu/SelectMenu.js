aced7e56dcb4c91d00e1958aedeaae7436f75840
Tayyib Cankat <tymnt410@gmail.com>
2021-06-17 20:38:05 +0300

    Test the packagezing

---
+++ dist/SelectMenu/SelectMenu.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+function SelectMenu() {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p1", null, "Menu"));
+}
+
+var _default = SelectMenu;
+exports.default = _default;
