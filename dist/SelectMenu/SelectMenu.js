fe6bb9ff642c457555920d662beda37275687470
Tayyib Cankat <tymnt410@gmail.com>
2021-06-17 20:47:42 +0300

    .

--- dist/SelectMenu/SelectMenu.js
+++ dist/SelectMenu/SelectMenu.js
@@ -5,8 +5,14 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+var _jsxRuntime = require("react/jsx-runtime");
+
 function SelectMenu() {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p1", null, "Menu"));
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    children: /*#__PURE__*/(0, _jsxRuntime.jsx)("p1", {
+      children: "Menu"
+    })
+  });
 }
 
 var _default = SelectMenu;
